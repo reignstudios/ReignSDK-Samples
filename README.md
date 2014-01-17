0ca4c5f1bfce0ebd9e8305409f25156c596080c0
Andrew Witte <zezba9000@gmail.com>
2014-01-17 03:05:28 -0500

    Starting to add Reign compiler app.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ReignSDK-Samples
 ================
+
+These are samples for the ReignSDK.
+
+The ReignSDK-Samples is copyright to Andrew Witte and Philip Witte and is under the BSD 2-Clause License.
+You should have received a copy of the BSD 2-Clause License along with this program.
+If not, see "licence.txt" or go to "http://www.opensource.org/licenses/bsd-license.php".
---
+++ ReignCompiler/Common/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Reign.Compiler;
+
+namespace ReignCompiler
+{
+	class Program
+	{
+static string CsToCpp =
+@"
+using System;
+
+namespace MyNamespace
+{
+	public class MyClass
+	{
+		private int index;
+
+		public void Foo(int index)
+		{
+			this.index = index;
+		}
+	}
+}
+";
+
+		static void Main(string[] args)
+		{
+			//if (args == null || args.Length == 0)
+			//{
+			//	Console.WriteLine("Program requires args...");
+			//	return;
+			//}
+
+			var compiler = new CppCompiler(CsToCpp, CompilerInputTypes.CsCode, CompilerOutputTypes.Cpp);
+			compiler.Compile("Output/");
+			Console.WriteLine("Done");
+			Console.ReadLine();
+		}
+	}
+}
